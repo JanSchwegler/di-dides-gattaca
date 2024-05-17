059a98bb4b8350ae72aeafb531e417120926ec01
Jan Schwegler <schwegler.jan@gmail.com>
2024-05-17 10:03:16 +0200

    basic setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # di-dides-gattaca
+
+# Links:
+## Figma:
+- https://www.figma.com/board/vFvGVkjqs6UG2wieWeTA9m/dides-pitch?node-id=0%3A1&t=bIZRLo9hJcJeHril-1
+- https://www.figma.com/design/AxuzlZXECePWj2fxL46TSs/Working-Title?node-id=0%3A1&t=rB9p6CdxvopLXobP-1
---
+++ assets/script.js
@@ -0,0 +1,36 @@
+// variables
+// variables - sizing and positioning
+let screenWidth = document.documentElement.clientWidth || window.innerWidth;
+let screenHeight = document.documentElement.clientHeight || window.innerHeight;
+let scrollPos = window.scrollY || window.pageYOffset;
+
+// on load
+document.addEventListener('DOMContentLoaded', function () {
+    // get elements
+
+    // event listeners
+
+    // function calls
+    
+});
+
+// on resize
+window.addEventListener("resize", function() {
+    // update screen width and height
+    screenWidth = document.documentElement.clientWidth || window.innerWidth;
+    screenHeight = document.documentElement.clientHeight || window.innerHeight;
+
+    // function calls
+    
+});
+
+// on scroll
+window.addEventListener("scroll", function() {
+    // update scroll position
+    scrollPos = window.scrollY || window.pageYOffset;
+
+    // function calls
+
+});
+
+// functions
